0a25b2b332816800b0835c499fc3aa935fa6cb80
ibrohim021 <dim_aviasky@mail.ru>
2022-08-09 22:44:35 +0300

    exempl-4

---
+++ exempl-4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, 
+// которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число ");
+
+string? numberString = Console.ReadLine();
+
+int number = int.Parse(numberString!);
+
+int count = 0;
+
+while(count < number)
+{
+
+    if(count%2==0)
+
+    Console.WriteLine(count);
+
+    count++;
+}
+
