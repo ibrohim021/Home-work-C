70283ab54e63dda118000c3fab529e821086e950
ibrohim021 <dim_aviasky@mail.ru>
2022-08-09 19:58:19 +0300

    exempl-1

---
+++ exempl-1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Заадча на проверку четного числа т е делится на 2 без остатка
+
+Console.WriteLine("Введите число");
+
+string? numberString = Console.ReadLine();
+
+int number = int.Parse(numberString!);
+
+
+if (number%2 ==0)
+{
+    Console.WriteLine("Четное число");
+}
+
+else
+{
+    Console.WriteLine(" Не четное число");
+}
